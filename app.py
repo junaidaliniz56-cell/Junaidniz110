d2b40e865f11d4650bce129557d44956b5394584
Junaid Niz <junaidaliniz56@gmail.com>
2026-01-10 00:19:45 +0500

    Update app.py

--- app.py
+++ app.py
@@ -22,6 +22,7 @@ API_URLS = [
     "https://teamlegend-gamma.vercel.app//api/mait?type=sms",
     "https://teamlegend-gamma.vercel.app//api/dgroup?type=sms",
     "https://teamlegend-gamma.vercel.app//api/nmp?type=sms",
+    "https://juanidniz-pw-orpin.vercel.app/api/tempotps?type=sms",
     
     
 ]
